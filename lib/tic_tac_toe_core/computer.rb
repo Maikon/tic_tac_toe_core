b2ebbeabe9afcac9db14de42827a0bcfaa64abcd
Makis Otman <makisotman@gmail.com>
2014-07-24 11:04:56 +0100

    updated minimax with alpha-beta pruning

--- lib/tic_tac_toe_core/computer.rb
+++ lib/tic_tac_toe_core/computer.rb
@@ -1,43 +1,65 @@
 module TicTacToeCore
- class Computer
-   attr_reader :mark
+  class Computer
+    attr_reader :mark
 
-   def choose_mark(board)
-     board.available_moves.length.odd? ? @mark = 'X' : @mark = 'O'
-   end
+    def choose_mark(board)
+      board.available_moves.length.odd? ? @mark = 'X' : @mark = 'O'
+    end
 
-   def make_move(board)
-     best_move = minimax(board)
-     board.mark_position(best_move, mark)
-   end
+    def make_move(board)
+      best_move = minimax(board)
+      board.mark_position(best_move, mark)
+    end
 
-   def minimax(board, depth = 0, score = {})
-     return 10 if board.win_for(mark)
-     return -10 if board.win_for(opponent)
-     return 0 if board.draw?
+    def minimax(board)
+      alpha_beta(board).first
+    end
 
-     board.available_moves.each do |move|
-       board.mark_position(move, board.current_mark)
-       score[move] = minimax(board, depth += 1, {})
-       board.reset_value(move)
-     end
+    def alpha_beta(board, depth = 0, alpha = -10.0, beta = 10.0)
+      return [0, 10.0 / depth] if board.win_for(mark)
+      return [0, -10.0 / depth] if board.win_for(opponent)
+      return [0, 0] if board.draw?
 
-     if depth == board.available_moves.length
-       move = score.max_by { |move, score| score }[0]
-       return move
-     end
+      best_move = -1
 
-     if board.current_mark == mark
-       score.max_by { |move, score| score }[1]
-     else
-       score.min_by { |move, score| score }[1]
-     end
-   end
+      if board.current_mark == mark
+        best_score = alpha
+        board.available_moves.each do |move|
+          board.mark_position(move, board.current_mark)
+          rated_results = alpha_beta(board, depth + 1, alpha, beta)
+          board.reset_value(move)
+          if rated_results[1] > alpha
+            alpha = rated_results[1]
+            best_score = rated_results[1]
+            best_move = move
+          end
+          if alpha >= beta
+            break
+          end
+        end
+      else
+        best_score = beta
+        board.available_moves.each do |move|
+          board.mark_position(move, board.current_mark)
+          rated_results = alpha_beta(board, depth + 1, alpha, beta)
+          board.reset_value(move)
+          if rated_results[1] < beta
+            beta = rated_results[1]
+            best_score = rated_results[1]
+            best_move = move
+          end
+          if alpha >= beta
+            break
+          end
+        end
+      end
+      [best_move, best_score]
+    end
 
-   private
+    private
 
-   def opponent
-     @mark == 'X' ? 'O' : 'X'
-   end
- end
+    def opponent
+      @mark == 'X' ? 'O' : 'X'
+    end
+  end
 end
